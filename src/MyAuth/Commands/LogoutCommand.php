fb7f8a08e1caac7f7562990360517b9c4952f225
Gabriel Gomez <30536237+ggomezz@users.noreply.github.com>
2017-08-12 10:57:58 -0500

    Update LogoutCommand.php

--- src/MyAuth/Commands/LogoutCommand.php
+++ src/MyAuth/Commands/LogoutCommand.php
@@ -1,28 +1,30 @@
 <?php
-
+ 
 namespace MyAuth\Commands;
-
+ 
 use MyAuth\MyAuth;
-
+ 
 use pocketmine\command\CommandExecutor;
 use pocketmine\command\CommandSender;
 use pocketmine\command\Command;
-
+ 
 use pocketmine\Player;
-
+ 
 class LogoutCommand implements CommandExecutor {
-
-	public function __construct(MyAuth $plugin) {
-		$this->plugin = $plugin;
-		$this->lang = $this->plugin->getLanguage();
-	}
-	
-	public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool {
-		if($sender instanceof Player){
-			$this->plugin->deauthorize($sender);
-			$sender->sendMessage($this->lang->getMessage('logout_success'));
-		} else {
-			$sender->sendMessage($this->lang->getMessage('ingame_only'));
-		}
-	}
+ 
+    public function __construct(MyAuth $plugin) {
+        $this->plugin = $plugin;
+        $this->lang = $this->plugin->getLanguage();
+    }
+   
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
+        if($sender instanceof Player){
+            $this->plugin->deauthorize($sender);
+            $sender->sendMessage($this->lang->getMessage('logout_success'));
+            return true;
+        } else {
+            $sender->sendMessage($this->lang->getMessage('ingame_only'));
+            return true;
+        }
+    }
 }
